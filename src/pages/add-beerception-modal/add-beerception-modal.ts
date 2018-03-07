d36f8d5e23aad12b4ca44058e238dc04f0d5a685
arandjel91 <arandjel91@gmail.com>
2018-03-07 12:30:58 +0100

    changed camera image type from base64 to image path url (saves memory consumption)

--- src/pages/add-beerception-modal/add-beerception-modal.ts
+++ src/pages/add-beerception-modal/add-beerception-modal.ts
@@ -84,17 +84,15 @@ export class AddBeerceptionModalPage {
   takePicture() {
     const options: CameraOptions = {
       quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
+      destinationType: this.camera.DestinationType.FILE_URI,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
     }
     
     this.camera.getPicture(options).then((imageData) => {
-     // imageData is either a base64 encoded string or a file URI
-     // If it's base64:
-     let base64Image = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
-     // Handle error
+      console.log(imageData);
+    }, (error) => {
+      console.log(error);
     });
   }
 
